5913c737638a4de57fa6a0f31997da7bf66dd60a
Zim92 <sebastian.otero@live.com.ar>
2018-08-23 22:26:12 -0300

    LUG Example Commit

---
+++ LUG01/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace LUG01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnAbrirBD = new System.Windows.Forms.Button();
+            this.btnCerrarBD = new System.Windows.Forms.Button();
+            this.txtIDPais = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(437, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(199, 158);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // btnAbrirBD
+            // 
+            this.btnAbrirBD.Location = new System.Drawing.Point(564, 158);
+            this.btnAbrirBD.Name = "btnAbrirBD";
+            this.btnAbrirBD.Size = new System.Drawing.Size(99, 58);
+            this.btnAbrirBD.TabIndex = 2;
+            this.btnAbrirBD.Text = "Abrir Base de Datos";
+            this.btnAbrirBD.UseVisualStyleBackColor = true;
+            this.btnAbrirBD.Click += new System.EventHandler(this.btnAbrirBD_Click);
+            // 
+            // btnCerrarBD
+            // 
+            this.btnCerrarBD.Location = new System.Drawing.Point(564, 241);
+            this.btnCerrarBD.Name = "btnCerrarBD";
+            this.btnCerrarBD.Size = new System.Drawing.Size(99, 58);
+            this.btnCerrarBD.TabIndex = 3;
+            this.btnCerrarBD.Text = "Cerrar Base de Datos";
+            this.btnCerrarBD.UseVisualStyleBackColor = true;
+            this.btnCerrarBD.Click += new System.EventHandler(this.btnCerrarBD_Click);
+            // 
+            // txtIDPais
+            // 
+            this.txtIDPais.Location = new System.Drawing.Point(153, 47);
+            this.txtIDPais.Name = "txtIDPais";
+            this.txtIDPais.Size = new System.Drawing.Size(100, 26);
+            this.txtIDPais.TabIndex = 4;
+            this.txtIDPais.TextChanged += new System.EventHandler(this.txtIDPais_TextChanged);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(153, 92);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 26);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(272, 47);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(99, 58);
+            this.btnAgregar.TabIndex = 6;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtIDPais);
+            this.Controls.Add(this.btnCerrarBD);
+            this.Controls.Add(this.btnAbrirBD);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnAbrirBD;
+        private System.Windows.Forms.Button btnCerrarBD;
+        private System.Windows.Forms.TextBox txtIDPais;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnAgregar;
+    }
+}
+
---
+++ LUG01/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LUG01
+{
+    public partial class Form1 : Form
+    {
+        //SqlConnection oCn;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        public void AbrirConexion()
+        {
+            //Clase que representa una conexion a una base de datos
+            SqlConnection oCn = new SqlConnection(@"Data Source=.\SQLEXPRESS;
+                                                Initial Catalog=LUG01; 
+                                                Integrated Security=True");
+            oCn.Open();
+
+            //Crea un comando. Clase que representa un comando SQL
+            SqlCommand oCmd = new SqlCommand();
+            //Establece la conexion que va a utilizar
+            oCmd.Connection = oCn;
+            //Establece como se interpretara el CommandText
+            oCmd.CommandType = CommandType.Text;
+            //Establece la instruccion SQL
+            oCmd.CommandText = "select * from Pais";
+            //Clase que representa una tabla de datos
+            DataTable data = new DataTable();
+            //Clase adaptadora entre un objeto DataSet y sus operaciones (select, insert, etc)
+            SqlDataAdapter adapter = new SqlDataAdapter(oCmd);
+            //Rellena un DataTable
+            adapter.Fill(data);
+            //Carga el DataTable en el DataGridView
+            dataGridView1.DataSource = data;
+
+
+            //DataSet es una superclase que contiene todas las tablas, vistas, relaciones
+            //integridad, etc. Copia la BD la extrae como XML y la guarda en un memoria
+            //para usar offline
+
+            //DataSet contiene DataTable, DataRelation, DataColumn, DataRow, Constraint
+
+            oCn.Close();
+            oCn.Dispose();
+            oCn = null;
+            GC.Collect();
+        }
+
+        private void CerrarConexion()
+        {
+            
+        }
+
+        private void Agregar()
+        {
+            //Clase que representa una conexion a una base de datos
+            SqlConnection oCn = new SqlConnection(@"Data Source=.\SQLEXPRESS;
+                                                Initial Catalog=LUG01; 
+                                                Integrated Security=True");
+            oCn.Open();
+            //Crea un comando. Clase que representa un comando SQL
+            SqlCommand oCmd = new SqlCommand();
+            //Establece la conexion que va a utilizar
+            oCmd.Connection = oCn;
+            //Establece como se interpretara el CommandText
+            oCmd.CommandType = CommandType.Text;
+            //Establece la instruccion SQL
+            oCmd.CommandText = "INSERT INTO Pais VALUES(@Id_Pais, @Nombre)";
+            //Clase que representa un parametro SQL
+            SqlParameter pId_Pais = new SqlParameter();
+            pId_Pais.ParameterName = "@Id_Pais";
+            pId_Pais.Value = Convert.ToInt32(txtIDPais.Text);
+            oCmd.Parameters.Add(pId_Pais);
+            //Clase que representa un parametro SQL
+            SqlParameter pNombre = new SqlParameter();
+            pNombre.ParameterName = "@Nombre";
+            pNombre.Value = txtNombre.Text.ToString();
+            oCmd.Parameters.Add(pNombre);
+            oCmd.ExecuteNonQuery();
+
+            txtIDPais.Clear();
+            txtNombre.Clear();
+
+            oCn.Close();
+            oCn.Dispose();
+            oCn = null;
+            GC.Collect();
+        }
+
+        private void btnAbrirBD_Click(object sender, EventArgs e)
+        {
+            AbrirConexion();
+        }
+
+        private void btnCerrarBD_Click(object sender, EventArgs e)
+        {
+            CerrarConexion();
+        }
+
+        private void txtIDPais_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            Agregar();
+        }
+
+        ///SqlConnection
+        ///Representa una conexion a un origen de datos SQL
+        ///Puede estar activa o cerrada.
+        ///SqlCommand
+        ///Representa un comando de SQL que puedde ejecutarse en una BD
+        ///DataTable
+        ///Clase que representa a una tabla de datos, contiene DataRow, DataColumn
+        ///DataSet
+        ///Clase que representa un conjunto de tablas, y sus relaciones. Representa
+        ///a una cache de una BD. Los datos se almacenan de forma temporal en la memoria
+        ///Puede usarse para trabajar sin conexion persistente
+        ///SqlDataAdapter
+        ///Se conecta a una BD y ejecuta instrucciones SQL, tanto para poblar un DataSet
+        ///como para actualizar el origen de datos del DataSet
+    }
+}
